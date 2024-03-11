fcde7829351a1015e3d1de2e6e35ba833d326b15
System Administrator <root@192.168.10.6>
2024-03-11 18:30:03 -0500

    Implement error message toast and enhance login with API.

--- src/presentation/hooks/use-auth-actions.ts
+++ src/presentation/hooks/use-auth-actions.ts
@@ -7,20 +7,32 @@ import { UserAuthAdapter } from '../../infrastructure/adapters/user-auth.adapter
 export const useUserAuthActions = () => {
 
   const dispatch = useAppDispatch();
-  const [ onLogin, { isLoading, error } ] = useOnLoginMutation();
+  const [ onLogin, { isLoading, isError, error } ] = useOnLoginMutation();
 
   const loginHandle = async (email: string, password: string) => {
     try {
       // Call the login API endpoint and await the response 
       const response: any = await onLogin({email, password})
       
-      // Adapt the user data from the response
-      const userAuthAdapter = await UserAuthAdapter(response.data)
+      console.log( '-------->>>>>>>', response ,  '<<<<<<<<------' );
+      console.log(isError );
+      console.log(error);
 
-      // Dispatch the login action with the adapted user data
-      await dispatch( login(userAuthAdapter) );
+      if (response.error) {
+        // ha ocurrido un error
+      }
+      
+      
+      // {"error": {"error": "TypeError: Network request failed", "status": "FETCH_ERROR"}}
 
-     
+
+      
+      // // Adapt the user data from the response
+      // const userAuthAdapter = await UserAuthAdapter(response.data);
+
+      // // Dispatch the login action with the adapted user data
+      // await dispatch( login(userAuthAdapter) );
+
     } catch (error) {
       // Handle any errors that occur during the login process
       console.error("Error login:", error);
